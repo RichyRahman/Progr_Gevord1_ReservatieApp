d5a4fb0637e3cc6c3083db690ebcd9b48df8d816
RichyRahman <126240550+RichyRahman@users.noreply.github.com>
2024-12-17 21:36:10 +0100

    Add files via upload

---
+++ ProgrGevord1_ReservatieApp/ProgrGevord1_ReservatieApp/BL/Park.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrGevord1_ReservatieApp.BL
+{
+    class Park
+    {
+        private int id;
+        private string naam;
+        private string locatie;
+
+        public Park()
+        {
+
+        }
+    }
+}
